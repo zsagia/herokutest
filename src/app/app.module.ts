285be79513924fb0da618fd96dca7ac6bb72f6a8
Zsolt Szabo <zsolt.szabo@liferay.com>
2017-03-18 19:15:06 +0100

    Added missing dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,20 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactService } from './contacts/contact.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactListComponent
+    ContactListComponent,
+    ContactDetailsComponent
   ],
+  exports: [
+      ContactListComponent,
+      ContactDetailsComponent
+    ],
   imports: [
     BrowserModule,
     FormsModule,
