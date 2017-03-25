650c921cf9affa69c0284efe3ec058e88edbc316
Zsolt Szabo <zsolt.szabo@liferay.com>
2017-03-25 08:49:24 +0100

    Added rounting module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { BuildingListComponent } from './building/building-list/building-list.component';
+import { MainViewComponent } from './main-view/main-view.component';
+import { BuildingAdminComponent } from './building/building-admin/building-admin.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/mainview', pathMatch: 'full' },
+  { path: 'mainview', component: MainViewComponent },
+  { path: 'buildinglist', component: BuildingListComponent },
+  { path: 'buildingadmin', component: BuildingAdminComponent }
+]
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+
+export class AppRoutingModule {}
