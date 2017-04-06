6adb0093929aa96a107359bb9d48aaab86d087de
Zsolt Szabo <zsolt.szabo@liferay.com>
2017-04-06 07:19:09 +0200

    SF

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule }             from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { BuildingListComponent } from './building/building-list/building-list.component';
@@ -7,20 +7,22 @@ import { BuildingAdminComponent } from './building/building-admin/building-admin
 import { UserAdminComponent } from './user/admin/user-admin.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LogInComponent } from './log-in/log-in.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/mainview', pathMatch: 'full' },
-  { path: 'mainview', component: MainViewComponent },
-  { path: 'buildinglist', component: BuildingListComponent },
-  { path: 'buildingadmin', component: BuildingAdminComponent },
-  { path: 'useradmin', component: UserAdminComponent },
-  { path: 'login', component: LogInComponent },
-  { path: 'signup', component: SignUpComponent }
+    { path: '', redirectTo: '/mainview', pathMatch: 'full' },
+    { path: 'mainview', component: MainViewComponent },
+    { path: 'buildinglist', component: BuildingListComponent },
+    { path: 'admin', component: AdminComponent },
+    { path: 'buildingadmin', component: BuildingAdminComponent },
+    { path: 'useradmin', component: UserAdminComponent },
+    { path: 'login', component: LogInComponent },
+    { path: 'signup', component: SignUpComponent }
 ]
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 
-export class AppRoutingModule {}
+export class AppRoutingModule { }
