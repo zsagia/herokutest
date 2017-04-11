c4c8d440ffa49fb82fdb7d56d67d79115b34f225
Zsolt Szabo <zsolt.szabo@liferay.com>
2017-04-11 07:51:08 +0200

    Added private path

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ function handleError(res, reason, message, code) {
  *    POST: creates a new building
  */
 
-app.get("/api/buildings", function (req, res) {
+app.get("/api/private/buildings", function (req, res) {
     db.collection(BUILDINGS_COLLECTION).find({}).toArray(function (err, docs) {
         if (err) {
             handleError(res, err.message, "Failed to get buildings.");
@@ -58,7 +58,7 @@ app.get("/api/buildings", function (req, res) {
     });
 });
 
-app.post("/api/buildings", function (req, res) {
+app.post("/api/private/buildings", function (req, res) {
     var newBuilding = req.body;
     newBuilding.createDate = new Date();
 
@@ -77,13 +77,7 @@ app.post("/api/buildings", function (req, res) {
     });
 });
 
-/*  "/api/buildings/:id"
- *    GET: find building by id
- *    PUT: update building by id
- *    DELETE: deletes building by id
- */
-
-app.get("/api/buildings/:id", function (req, res) {
+app.get("/api/private/buildings/:id", function (req, res) {
     db.collection(BUILDINGS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function (err, doc) {
         if (err) {
             handleError(res, err.message, "Failed to get building");
@@ -93,7 +87,7 @@ app.get("/api/buildings/:id", function (req, res) {
     });
 });
 
-app.put("/api/buildings/:id", function (req, res) {
+app.put("/api/private/buildings/:id", function (req, res) {
     var updateDoc = req.body;
     delete updateDoc._id;
 
@@ -107,22 +101,12 @@ app.put("/api/buildings/:id", function (req, res) {
     });
 });
 
-app.delete("/api/buildings/:id", function (req, res) {
+app.delete("/api/private/buildings/:id", function (req, res) {
     db.collection(BUILDINGS_COLLECTION).deleteOne({ _id: new ObjectID(req.params.id) }, function (err, result) {
         if (err) {
             handleError(res, err.message, "Failed to delete building");
         } else {
             res.status(200).json(req.params.id);
-        }
-    });
-});
-
-app.get("/api/users", function (req, res) {
-    db.collection(USERS_COLLECTION).find({}).toArray(function (err, docs) {
-        if (err) {
-            handleError(res, err.message, "Failed to get users.");
-        } else {
-            res.status(200).json(docs);
         }
     });
 });
@@ -146,7 +130,36 @@ app.post("/api/users", function (req, res) {
     });
 });
 
-app.get("/api/users/:id", function (req, res) {
+app.get("/api/private/users", function (req, res) {
+    db.collection(USERS_COLLECTION).find({}).toArray(function (err, docs) {
+        if (err) {
+            handleError(res, err.message, "Failed to get users.");
+        } else {
+            res.status(200).json(docs);
+        }
+    });
+});
+
+app.post("/api/private/users", function (req, res) {
+    var newUser = req.body;
+    newUser.createDate = new Date();
+
+    delete newUser._id;
+
+    if (!req.body.userName) {
+        handleError(res, "Invalid user input", "Must provide a userName.", 400);
+    }
+
+    db.collection(USERS_COLLECTION).insertOne(newUser, function (err, doc) {
+        if (err) {
+            handleError(res, err.message, "Failed to create new user.");
+        } else {
+            res.status(201).json(doc.ops[0]);
+        }
+    });
+});
+
+app.get("/api/private/users/:id", function (req, res) {
     db.collection(USERS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function (err, doc) {
         if (err) {
             handleError(res, err.message, "Failed to get user");
@@ -156,7 +169,7 @@ app.get("/api/users/:id", function (req, res) {
     });
 });
 
-app.put("/api/users/:id", function (req, res) {
+app.put("/api/private/users/:id", function (req, res) {
     var updateDoc = req.body;
     delete updateDoc._id;
 
@@ -170,7 +183,7 @@ app.put("/api/users/:id", function (req, res) {
     });
 });
 
-app.delete("/api/users/:id", function (req, res) {
+app.delete("/api/private/users/:id", function (req, res) {
     db.collection(USERS_COLLECTION).deleteOne({ _id: new ObjectID(req.params.id) }, function (err, result) {
         if (err) {
             handleError(res, err.message, "Failed to delete user");
@@ -195,9 +208,9 @@ app.post("/api/authenticate", function (request, response) {
         if (error) {
             handleError(response, error.message, "Failed to login user.");
         } else if (!user) {
-            handleError(response, error.message, "User not found.");
+            handleError(response, "error", "No user with the given credentials!");
         } else if (user.password != body.password) {
-            handleError(response, error.message, "Wrong password.");
+            handleError(response, "error", "The given password is not correct");
         } else {
             var token = jwt.sign(
                 user,
